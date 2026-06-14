6d031ecd83c669c3c723910318afbeeed904f09c
mykyta <380630700519b@gmail.com>
2026-06-14 11:34:08 +0200

    Add simple logic

--- src/main/java/com/study/note_service/services/NoteService.java
+++ src/main/java/com/study/note_service/services/NoteService.java
@@ -1,7 +1,24 @@
 package com.study.note_service.services;
 
+import com.study.note_service.DTO.request.CreateNotesRequest;
+import com.study.note_service.NotesRepository;
+import com.study.note_service.entity.NotesEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NoteService {
+
+    private final NotesRepository notesRepository;
+
+    public NoteService(NotesRepository notesRepository) {
+        this.notesRepository = notesRepository;
+    }
+
+    public void createNote(CreateNotesRequest request){
+        NotesEntity note = new NotesEntity();
+        note.setTitle(request.title());
+        note.setContent(request.content());
+        notesRepository.save(note);
+    }
+
 }
