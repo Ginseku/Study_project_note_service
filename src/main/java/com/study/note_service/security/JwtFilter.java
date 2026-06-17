ebc44ca6b8d3ef6aa256c33b2a4b429a60f2d409
mykyta <380630700519b@gmail.com>
2026-06-17 21:29:05 +0200

    Change JWT filter( fix bag with 403)

--- src/main/java/com/study/note_service/security/JwtFilter.java
+++ src/main/java/com/study/note_service/security/JwtFilter.java
@@ -7,10 +7,13 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Collections;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
@@ -27,26 +30,36 @@ public class JwtFilter extends OncePerRequestFilter {
             HttpServletResponse response,
             FilterChain filterChain
     ) throws ServletException, IOException {
+
         String header = request.getHeader("Authorization");
+
         if (header == null || !header.startsWith("Bearer ")) {
-            response.setStatus(401);
+            filterChain.doFilter(request, response);
             return;
         }
+
         try {
             String token = header.substring(7);
+
             Claims claims = jwtService.extractClaims(token);
-            request.setAttribute("userId", claims.get("userId", Long.class));
-        }
-        catch (Exception ex) {
-            response.setStatus(401);
+
+            Long userId = claims.get("userId", Long.class);
+
+            UsernamePasswordAuthenticationToken auth =
+                    new UsernamePasswordAuthenticationToken(
+                            userId,
+                            null,
+                            Collections.emptyList()
+                    );
+
+            SecurityContextHolder.getContext().setAuthentication(auth);
+
+        } catch (Exception ex) {
+            filterChain.doFilter(request, response);
             return;
         }
 
-        filterChain.doFilter(
-                request,
-                response
-        );
-
+        filterChain.doFilter(request, response);
     }
 
 }
